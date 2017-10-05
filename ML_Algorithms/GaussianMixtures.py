dcdc687edabd65a0628865e24d6b150457575874
joseluisvaz <joseluisv1201@gmail.com>
2017-10-05 16:10:19 -0500

    Implementation of Multivariate Gaussian Distribution

--- ML_Algorithms/GaussianMixtures.py
+++ ML_Algorithms/GaussianMixtures.py
@@ -19,6 +19,35 @@ class UnivariateGaussian:
         return 'Gaussian({0:4.6}, {1:4.6})'.format(self.mu, self.sigma)
 
 
+class MultivariateGaussian:
+    """
+    Implementation of Multivariate Gaussian Distribution
+    """
+    def __init__(self, mu, sigma):
+        """
+        Initialization of a Multivariate Gaussian distribution, we do precomputation to
+        avoid extra computation in further methods.
+        :param mu: Vertical vector of the mean
+        :param sigma: Covariance Matrix
+        """
+        self.mu = mu
+        self.sigma = sigma
+        self.inv_sigma = np.linalg.pinv(sigma)
+        self.det_sigma = np.linalg.det(sigma)
+
+    def pdf(self, x):
+        """
+        Estimating probability at a single point
+        :param x: datapoint must be the same size as mu
+        :return: probability of this datapoint
+        """
+        u = (x - self.mu)
+        z = - 1/2.0 * np.dot(np.dot(u.T, self.inv_sigma), u)
+        norm_factor = (1/((2*np.pi)**(self.mu.shape[0]/2.0)))*(1/np.sqrt(self.det_sigma))
+        p_x = norm_factor * np.exp(z)
+
+        return p_x
+
 class GaussianMixture:
     """
     Model Mixture of n multivariate Gaussian Distributions
