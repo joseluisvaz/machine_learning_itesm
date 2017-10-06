6cd6244140999ebe4d7a9dbe36b4b101a3cd45ed
joseluisvaz <joseluisv1201@gmail.com>
2017-10-05 19:41:17 -0500

    Expectation Step implemented, testinng needs to be done

--- ML_Algorithms/GaussianMixtures.py
+++ ML_Algorithms/GaussianMixtures.py
@@ -40,12 +40,14 @@ class MultivariateGaussian:
         :param x: datapoint must be the same size as mu
         :return: probability of this datapoint
         """
+        if x.shape != (x.shape[0], 1):
+            x = x.reshape((x.shape[0], 1))
         u = (x - self.mu)
         z = - 1/2.0 * np.dot(np.dot(u.T, self.inv_sigma), u)
         norm_factor = (1/((2*np.pi)**(self.mu.shape[0]/2.0)))*(1/np.sqrt(self.det_sigma))
         p_x = norm_factor * np.exp(z)
 
-        return p_x
+        return float(p_x)
 
 class GaussianMixture:
     """
@@ -55,7 +57,7 @@ class GaussianMixture:
     def __init__(self,
                  data,
                  clusters=2,
-                 mix=0.5
+                 mix=(0.3333, 0.3333, 0.3333)
                  ):
         """
         Random initialization of mean vector,
@@ -66,13 +68,48 @@ class GaussianMixture:
         """
 
         self.data = data
+        self.clusters = clusters
         self.dists = {}
         self.mix = mix
+        self.log_likelihood = 0
         mins = np.min(data, axis=0)
         maxs = np.max(data, axis=0)
         for i in range(clusters):
             self.dists["dist" + str(i)] = MultivariateGaussian(random.uniform(mins, maxs).reshape(mins.shape[0], 1),
                                                                np.identity(mins.shape[0]))
+
+    def expectation(self):
+        """
+        Expectation Step for Gaussian Mixtures
+        :return: Parameters
+        """
+        # Initializing dictionary to store probabilities of n clusters
+        prob_z = {}
+        probs = np.zeros(self.data[0], 1)
+
+        for i in range(self.clusters):
+            probs["cluster" + str(i)] = np.zeros((self.data.shape[0], 1))
+
+        for n in range(self.data.shape[0]):
+
+            for i in range(self.clusters):
+                prob_z["cluster" + str(i)] = self.dists["dist" + str(i)].pdf(self.data[n]) * self.mix[0]
+
+            # Normalization Factor
+            den = sum(prob_z.values())
+            # Normalize!
+            sume = 0
+            for i in range(self.clusters):
+                prob_z["cluster" + str(i)] /= den
+                sume += prob_z["cluster" + str(i)]
+
+            self.log_likelihood += np.log(sume)
+
+            for i in range(self.clusters):
+                probs["cluster" + str(i)][n] = prob_z["cluster" + str(i)]
+
+        return probs
+
 
 
 
